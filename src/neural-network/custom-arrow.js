2cb44c92b894b1eb6d866799a01ead3e997c7823
mathadoor <matharooh2@gmail.com>
2023-06-24 15:13:49 -0400

    updated architecture

---
+++ src/neural-network/custom-arrow.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Xarrow from "react-xarrows";
+
+class OutputLayer extends React.Component {
+  render() {
+    // eslint-disable-next-line no-unused-vars
+
+    return (
+      <rect {...this.props}/>
+    );
+  }
+}
+
+export default OutputLayer;
